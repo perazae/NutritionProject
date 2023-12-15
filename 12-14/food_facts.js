e56709df995ed94f2f76777b71f2a50b538b8cad
perazae <edsonperaza160@gmail.com>
2023-12-15 14:34:08 -0800

    worked on js

--- 12-14/food_facts.js
+++ 12-14/food_facts.js
@@ -1,20 +1,42 @@
+"use strict";
 const baseURL = "https://world.openfoodfacts.org/api/v2/product/";
 
 async function fetchFoodFacts() {
   const endpointURL = document.getElementById("barcode").value;
   if (!endpointURL) {
-    console.error("Barcode is not provided");
+    const message = document.createElement('p')
+    message.innerHTML = "Barcode is not provided"
     return;
   }
-
+  const languageParam = "?tags_lc=en";
   try {
-    const response = await fetch(baseURL + endpointURL);
+    const response = await fetch(baseURL + endpointURL + languageParam);
     if (!response.ok) {
       throw new Error("Network response was not ok");
     }
     const data = await response.json();
-    console.log(data);
+    displayFoodData(data);
   } catch (error) {
     console.error("There was a problem with the fetch operation:", error);
   }
+}
+
+function displayFoodData(data) {
+  const displayProduct = document.getElementById("displayProduct");
+  
+  displayProduct.innerHTML = "";
+  
+  const nameParagraph = document.createElement("p");
+  nameParagraph.textContent = `Product Name: ${data.product.product_name_en}`;
+  displayProduct.appendChild(nameParagraph);
+
+  const fatParagraph = document.createElement("p");
+  nameParagraph.textContent = `Fat: ${data.product.nutriments["fat"]}`;
+  displayProduct.append(fatParagraph);
+
+  const ingredientsParagraph = document.createElement("p");
+  ingredientsParagraph.textContent = `Ingredients: ${data.product.ingredients_text}`;
+  displayProduct.appendChild(ingredientsParagraph);
+
+
 }
--- 12-14/users.js
+++ 12-14/users.js
@@ -5,10 +5,27 @@ function fetchClassData() {
 
   fetch(url)
     .then((response) => response.json())
-    .then((data) => displayClassData(data))
+    .then((data) => {
+      data.forEach((studentData) => {
+        const card = `
+          <div class="card m-3" style="width: 18rem;">
+            <img src="${studentData.imageURL}" class="card-img-top" alt="Image of ${studentData.Name}">
+            <div class="card-body">
+              <h5 class="card-title">${studentData.Name}</h5>
+              <p class="card-text"><span class="fw-bold">Email: </span>${studentData.Email}</p>
+              <p class="card-text"><span class="fw-bold">Nickname: </span>${studentData.CodingNickname}</p>
+              <p class="card-text"><span class="fw-bold">Group: </span>${studentData.Groupname}</p>
+              <a href="${studentData.GitHubProfile}" class="btn btn-primary" target="_blank">Github</a>
+            </div>
+          </div>
+        `;
+
+        // Assuming there is a container element with id "studentContainer" to append the cards
+        document.getElementById("studentContainer").innerHTML += card;
+      });
+    })
     .catch((error) => console.log("Error fetching class data"));
 }
-
 function displayClassData(classData) {
   const classDiv = document.getElementById("classData");
   classData.forEach((student) =>
@@ -16,18 +33,4 @@ function displayClassData(classData) {
   );
 }
 
-function createStudentCard(studentData) {
-  const card = `
-    <div class="card m-3" style="width: 18rem;">
-      <img src="${studentData.imageURL}" class="card-img-top" alt="Image of ${studentData.Name}">
-      <div class="card-body">
-        <h5 class="card-title">${studentData.Name}</h5>
-        <p class="card-text"><span class="fw-bold">Email: </span>${studentData.Email}</p>
-        <p class="card-text"><span class="fw-bold">Nickname: </span>${studentData.CodingNickname}</p>
-        <p class="card-text"><span class="fw-bold">Group: </span>${studentData.Groupname}</p>
-        <a href="${studentData.GitHubProfile}" class="btn btn-primary" target="_blank">Github</a>
-      </div>
-    </div>
-  `;
-  return card;
-}
+
