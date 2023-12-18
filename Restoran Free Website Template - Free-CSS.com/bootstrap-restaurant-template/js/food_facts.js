42b79f749be05000471fe2f7f91237683e6ab3ee
Derek <liderek63@gmail.com>
2023-12-18 10:36:13 -0800

    add spinner when fetching data

--- Restoran Free Website Template - Free-CSS.com/bootstrap-restaurant-template/js/food_facts.js
+++ Restoran Free Website Template - Free-CSS.com/bootstrap-restaurant-template/js/food_facts.js
@@ -4,11 +4,20 @@ const baseURL = "https://world.openfoodfacts.net/api/v3/product/";
 async function fetchFoodFacts() {
   const endpointURL = document.getElementById("barcode").value;
   if (!endpointURL) {
-    const message = document.createElement('p')
-    message.innerHTML = "Barcode is not provided"
+    const message = document.createElement("p");
+    message.innerHTML = "Barcode is not provided";
     return;
   }
   const languageParam = "?tags_lc=en";
+  const productInfo = document.getElementById("productInfo");
+  productInfo.classList.remove("d-none");
+
+  const displayProduct = document.getElementById("displayProduct");
+  displayProduct.classList.remove("d-none");
+
+  const spinnerInfo = document.getElementById("spinnerInfo");
+  spinnerInfo.classList.remove("d-none");
+
   try {
     const response = await fetch(baseURL + endpointURL + languageParam);
     if (!response.ok) {
@@ -20,40 +29,34 @@ async function fetchFoodFacts() {
   } catch (error) {
     console.error("There was a problem with the fetch operation:", error);
   }
+
+  spinnerInfo.classList.add("d-none");
 }
 
 function displayFoodData(data) {
   const displayProduct = document.getElementById("displayProduct");
-  const productInfo = document.getElementById("productInfo");
-  productInfo.classList.remove("d-none");
   displayProduct.innerHTML = "";
 
   const containerElement = document.createElement("div");
   containerElement.id = "imageContainer";
   const productImage = document.createElement("img");
-  productImage.src = `${data.product.image_front_small_url}`
+  productImage.src = `${data.product.image_front_small_url}`;
   containerElement.appendChild(productImage);
   displayProduct.appendChild(containerElement);
-
-
-
 
   const carbsParagraph = document.createElement("p");
   carbsParagraph.innerHTML = `<strong>Carbs:</strong> ${data.product.nutriments["carbohydrates"]}`;
   displayProduct.appendChild(carbsParagraph);
-  
+
   const fatParagraph = document.createElement("p");
   fatParagraph.innerHTML = `<strong>Fat:</strong> ${data.product.nutriments["fat"]}`;
   displayProduct.appendChild(fatParagraph);
-  
+
   const proteinParagraph = document.createElement("p");
   proteinParagraph.innerHTML = `<strong>Protein:</strong> ${data.product.nutriments["proteins"]}`;
   displayProduct.appendChild(proteinParagraph);
-  
-  const ingredientsParagraph = document.createElement("p");
-  ingredientsParagraph.innerHTML = `<strong>Ingredients:</strong> ${data.product.ingredients_text}`;
-  displayProduct.appendChild(ingredientsParagraph);
-  
- 
 
+  const ingredientsParagraph = document.createElement("p");
+  ingredientsParagraph.innerHTML = `<strong>Ingredients:</strong> ${data.product["ingredients_text"]}`;
+  displayProduct.appendChild(ingredientsParagraph);
 }
